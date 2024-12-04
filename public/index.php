37b8ee0623ada239141b9bd1ca79eaff256eb386
Danver <danver.ver@hotmail.com>
2024-12-04 11:07:07 -0300

    Configurações iniciais do index

--- public/index.php
+++ public/index.php
@@ -1,1 +1,29 @@
 <?php
+
+// Iniciando a sessão
+
+session_start();
+
+// Controlando o acesso a aplicação
+
+define('CONTROL', true);
+
+// Verificando se há algum usuário logado
+
+$usuario_logado = $_SESSION['usuario'] ?? null;
+
+// Verificando qual a rota na URL
+
+if (empty($usuario_logado)) {
+    $rota = 'login';
+} else {
+    $rota = $_GET['rota'] ?? 'home';
+}
+
+// Analisando a rota
+
+$rotas =
+    [
+        'login' => 'login.php',
+        'home' => 'home.php'
+    ];
