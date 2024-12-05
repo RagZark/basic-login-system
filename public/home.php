91ef493d1af88cc52b5b6a04f89cb54e4273ee04
Danver <danver.ver@hotmail.com>
2024-12-05 10:23:14 -0300

    Tratamento de erros e login

---
+++ public/home.php
@@ -0,0 +1,18 @@
+<?php
+defined('CONTROL') or die('Acesso negado!');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <h3>Estou dentro da aplicação!</h3>
+</body>
+
+</html>
--- public/login.php
+++ public/login.php
@@ -1,5 +1,36 @@
 <?php
 defined('CONTROL') or die('Acesso negado!');
+
+// Verificar se o formulário foi submetido
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Verificar se o usuário e senha foram submetidos
+    $usuario = $_POST['usuario'] ?? null;
+    $senha = $_POST['senha'] ?? null;
+    $erro = null;
+
+    if (empty($usuario) || empty($senha)) {
+        $erro = 'Usuário e senha são obrigatórios!';
+    }
+
+    // Verificando se o usuário e senha são válidos (match)
+    if (empty($erro)) {
+        $usuarios = require_once __DIR__ . '/../inc/usuarios.php';
+
+        foreach ($usuarios as $user) {
+            if ($user['usuario'] == $usuario && password_verify($senha, $user['senha'])) {
+                // Efetuar Login
+                $_SESSION['usuario'] = $usuario;
+
+                // Voltar a página inicial
+                header('location: index.php?rota=home');
+            }
+        }
+
+        // Login invalido
+        $erro = "Usuário e/ou senha inválidos";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +59,9 @@ defined('CONTROL') or die('Acesso negado!');
             </div>
         </form>
     </div>
+    <?php if (!empty($erro)) : ?>
+        <p style="color:red"><?= $erro ?></p>
+    <?php endif; ?>
 </body>
 
 </html>
